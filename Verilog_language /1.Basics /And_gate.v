2dca8c6201f27f0ae5db6b5bf3e1c4e0a91f9399
Aruna Annadurai <arunaparimala76@gmail.com>
2026-04-10 18:27:55 +0530

    Create And_gate.v

---
+++ Verilog_language /1.Basics /And_gate.v
@@ -0,0 +1,13 @@
+module top_module( 
+    input a, 
+    input b, 
+    output out );
+    assign out=a&b;
+endmodule
+
+/*module top_module( 
+    input a, 
+    input b, 
+    output out );
+    and(out,a,b);
+endmodule*/
