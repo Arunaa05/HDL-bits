75b560dc1b25d7cc2d40b0554359a746500c047c
Aruna Annadurai <arunaparimala76@gmail.com>
2026-04-11 13:27:21 +0530

    Create Module.v

---
+++ Verilog_language /3.Modules_Hierarchy /Module.v
@@ -0,0 +1,7 @@
+module top_module ( input a, input b, output out );
+    mod_a instance1(.out(out),.in1(a),.in2(b));
+endmodule
+
+/*module top_module ( input a, input b, output out );
+    mod_a instance1(a,b,out);
+endmodule*/
