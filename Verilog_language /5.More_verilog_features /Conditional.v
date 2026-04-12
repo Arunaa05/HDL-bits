e881fb4788fd4f8ca871def75b771251766e4f56
Aruna Annadurai <arunaparimala76@gmail.com>
2026-04-12 12:10:50 +0530

    Update Conditional.v

--- Verilog_language /5.More_verilog_features /Conditional.v
+++ Verilog_language /5.More_verilog_features /Conditional.v
@@ -1,1 +1,18 @@
+module top_module (
+    input [7:0] a, b, c, d,
+    output [7:0] min
+);
+    wire [7:0]e,f;
+    assign e=(a<b)?a:b;
+    assign f=(e<c)?e:c;
+    assign min=(f<d)?f:d;
+  
+endmodule
 
+/*module top_module (
+    input [7:0] a, b, c, d,
+    output [7:0] min
+);
+    assign min=((a<b && a<c && a<d)?a:(b<c && b<d)?b:(c<d)?c:d);
+  
+endmodule
