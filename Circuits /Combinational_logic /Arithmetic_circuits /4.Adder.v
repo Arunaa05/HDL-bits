44df4b7d5880eef576e96f8129a6e8e7ab46a88d
Aruna Annadurai <arunaparimala76@gmail.com>
2026-04-13 10:28:29 +0530

    Create 4.Adder.v

---
+++ Circuits /Combinational_logic /Arithmetic_circuits /4.Adder.v
@@ -0,0 +1,22 @@
+module full_adder(
+    input x,y,
+    input cin,
+    output sum,
+    output cout
+);
+    assign sum =x^y^cin;
+    assign cout=(x&y)|(y&cin)|(x&cin);
+    
+endmodule
+module top_module (
+    input [3:0] x,
+    input [3:0] y, 
+    output [4:0] sum
+);
+    wire c0,c1,c2;
+    full_adder fa1(.x(x[0]),.y(y[0]),.cin(0),.sum(sum[0]),.cout(c0));
+    full_adder fa2(.x(x[1]),.y(y[1]),.cin(c0),.sum(sum[1]),.cout(c1));
+    full_adder fa3(.x(x[2]),.y(y[2]),.cin(c1),.sum(sum[2]),.cout(c2));
+    full_adder fa4(.x(x[3]),.y(y[3]),.cin(c2),.sum(sum[3]),.cout(sum[4]));
+    
+endmodule
