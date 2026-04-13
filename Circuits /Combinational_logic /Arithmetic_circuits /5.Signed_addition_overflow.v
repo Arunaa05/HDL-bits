73b1f6d623332f82b36f333008426ae39a5e1d55
Aruna Annadurai <arunaparimala76@gmail.com>
2026-04-13 10:45:20 +0530

    Create 5.Signed_addition_overflow.v

---
+++ Circuits /Combinational_logic /Arithmetic_circuits /5.Signed_addition_overflow.v
@@ -0,0 +1,23 @@
+module top_module (
+    input [7:0] a,
+    input [7:0] b,
+    output [7:0] s,
+    output overflow
+); 
+
+     assign s = a + b;
+     assign overflow = (a[7] & b[7] & ~s[7])|(~a[7] & ~b[7] & s[7]);
+      
+endmodule
+
+/*module top_module (
+    input [7:0] a,
+    input [7:0] b,
+    output [7:0] s,
+    output overflow
+); 
+
+     assign s = a + b;
+    assign overflow = (a[7]==b[7])&&(s[7]!=b[7]);
+      
+endmodule*/
