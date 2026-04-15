f1659ab53e1604d426b893af49dc803c39ccf671
Aruna Annadurai <arunaparimala76@gmail.com>
2026-04-15 10:08:25 +0530

    Create 1.Shift4.v

---
+++ Circuits /Sequential_logic /Shift_Registers /1.Shift4.v
@@ -0,0 +1,23 @@
+module top_module(
+    input clk,
+    input areset,  // async active-high reset to zero
+    input load,
+    input ena,
+    input [3:0] data,
+    output reg [3:0] q
+); 
+    always@(posedge clk or posedge areset)
+        begin
+            if(areset)
+                q<=0;
+            else if(load==1)
+                q<=data;
+            else if(ena)
+                begin 
+                     for(int i=0;i<3;i++)
+                            q[i]=q[i+1];
+                            q[3]<=0;
+                end     
+        end
+
+endmodule
