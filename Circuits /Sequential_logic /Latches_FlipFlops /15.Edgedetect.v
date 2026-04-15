a804bf40f22e0ae88c57ef47a6608cc55eeeeeb4
Aruna Annadurai <arunaparimala76@gmail.com>
2026-04-15 10:35:37 +0530

    Update 15.Edgedetect.v

--- Circuits /Sequential_logic /Latches_FlipFlops /15.Edgedetect.v
+++ Circuits /Sequential_logic /Latches_FlipFlops /15.Edgedetect.v
@@ -3,6 +3,31 @@ module top_module (
     input [7:0] in,
     output [7:0] pedge
 );
+    integer i;
+    wire [7:0]w;
+    always@(posedge clk)
+        begin
+            for(i=0;i<8;i++)
+                begin
+                if(w[i]==0 && in[i]==1)
+                    pedge[i]<=1;
+                else
+                    pedge[i]<=0;
+                end
+            w<=in;
+        end
+endmodule
+
+                
+            
+
+
+
+/*module top_module (
+    input clk,
+    input [7:0] in,
+    output [7:0] pedge
+);
     wire [7:0]w;
     always@(posedge clk)
         begin
@@ -10,4 +35,4 @@ module top_module (
             pedge<=~w & in;
         end
 
-endmodule
+endmodule*/
