98f299c55a5dcc56947af9c9f5a9ad2659024c6c
Aruna Annadurai <arunaparimala76@gmail.com>
2026-04-15 10:39:16 +0530

    Update Popcount255.v

--- Verilog_language /5.More_verilog_features /Popcount255.v
+++ Verilog_language /5.More_verilog_features /Popcount255.v
@@ -2,11 +2,12 @@ module top_module(
     input [254:0] in,
     output [7:0] out
 );
+    integer i;
     reg [7:0]count;
     always@(in)
         begin
             count=0;
-            for(int i=0;i<255;i++)
+            for(i=0;i<255;i++)
                 begin
                     if(in[i]==1'b1)
                         count =count+1'b1;
