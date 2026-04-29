42ba917d8b99d2c3a86ddbf9da96fe2867321041
Aruna Annadurai <arunaparimala76@gmail.com>
2026-04-29 23:08:32 +0530

    Create Shift_register_and_down_counter.v

---
+++ Circuits /Building_larger_circuits /Shift_register_and_down_counter.v
@@ -0,0 +1,18 @@
+module top_module (
+    input clk,
+    input shift_ena,
+    input count_ena,
+    input data,
+    output [3:0] q);
+    reg[3:0]q1;
+    always@(posedge clk)begin
+        if(shift_ena)begin
+           q1<={q1[2:0],data}; 
+        end
+        else if(count_ena)begin
+            q1<=q1-1;
+        end
+    end
+    assign q=q1;
+
+endmodule
